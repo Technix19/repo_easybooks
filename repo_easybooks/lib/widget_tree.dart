5ebda73c16b98270790feb7d14b1466d29c97cec
ChompD <toribiodexterc@gmail.com>
2025-10-18 14:34:18 +0800

    import expenses

--- repo_easybooks/lib/widget_tree.dart
+++ repo_easybooks/lib/widget_tree.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:repo_easybooks/auth/auth_gate.dart';
 import 'package:repo_easybooks/auth/auth_service.dart';
 import 'package:repo_easybooks/data/notifiers.dart';
+import 'package:repo_easybooks/expenses_page.dart';
 import 'package:repo_easybooks/home_page.dart';
 import 'package:repo_easybooks/income_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
