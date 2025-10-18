78093f72fcc0b63cec4ec5d125f4798ea6ddcf1d
ChompD <toribiodexterc@gmail.com>
2025-10-18 14:37:55 +0800

    fixed the expenses not reading the formula

--- repo_easybooks/lib/expenses_page.dart
+++ repo_easybooks/lib/expenses_page.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: deprecated_member_use
 import 'dart:math' as math;
-import 'package:easybooks/data/export_table_as_csv.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:easybooks/formula.dart';
 import 'package:flutter/material.dart';
+import 'package:repo_easybooks/data/export_table_as_csv.dart';
+import 'package:repo_easybooks/formula.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:intl/intl.dart';
 
