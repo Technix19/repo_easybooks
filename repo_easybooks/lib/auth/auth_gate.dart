0ad2865e9dadebf5309da596f39b78d637c371d8
Technix19 <lanceapostol04@gmail.com>
2025-10-18 13:54:52 +0800

    Add Authentication Services and Gate

---
+++ repo_easybooks/lib/auth/auth_gate.dart
@@ -0,0 +1,26 @@
+import 'package:easybooks/login_page.dart';
+import 'package:easybooks/widget_tree.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<AuthState>(
+      stream: Supabase.instance.client.auth.onAuthStateChange,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        final session =
+            snapshot.data?.session ??
+            Supabase.instance.client.auth.currentSession;
+        return session != null ? const WidgetTree() : const LoginPage();
+      },
+    );
+  }
+}
---
+++ repo_easybooks/lib/auth/auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  //Sign In
+  Future<AuthResponse> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    return await _supabase.auth.signInWithPassword(
+      password: password,
+      email: email,
+    );
+  }
+
+  //Sign Up
+  Future<AuthResponse> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    return await _supabase.auth.signUp(password: password, email: email);
+  }
+
+  //Sign Out
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  // Get Current User's Email
+  String? getCurrentUserEmail() {
+    final session = _supabase.auth.currentSession;
+    final user = session?.user;
+    return user?.email;
+  }
+}
