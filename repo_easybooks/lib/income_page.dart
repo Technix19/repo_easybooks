bfa513e3ec1f74729a10d535686f9fb5f07f2c9a
Technix19 <lanceapostol04@gmail.com>
2025-10-18 14:18:17 +0800

    Finish Income Page

--- repo_easybooks/lib/income_page.dart
+++ repo_easybooks/lib/income_page.dart
@@ -4,6 +4,7 @@ import 'dart:math' as math;
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:repo_easybooks/data/export_table_as_csv.dart';
+import 'package:repo_easybooks/formula.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:intl/intl.dart';
 
